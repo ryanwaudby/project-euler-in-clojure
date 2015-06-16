b8951b43ce860325da4231ddd13b36894503906c
Ryan Lewis Waudby <ryan@ryanwaudby.co.uk>
2015-06-16 20:06:19 +0100

    updated problem 1

--- src/project_euler_in_clojure/problem_1.clj
+++ src/project_euler_in_clojure/problem_1.clj
@@ -1,6 +1,3 @@
 (ns project-euler-in-clojure.problem-1)
 
-(defn problem1
-  []
-  (reduce + (set (concat (range 0 1000 3) (range 0 1000 5))))
-  )
+(defn problem1 [] (reduce + (set (concat (range 0 1000 3) (range 0 1000 5)))))
