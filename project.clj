01ff4f068bd77f561fd9c86daf44d83367373fe6
Ryan Lewis Waudby <ryan@ryanwaudby.co.uk>
2015-06-16 17:19:06 +0100

    updated project def

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject project-euler-in-clojure "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Project Euler problems in Clojure"
+  :url "https://github.com/ryanwaudby/project-euler-in-clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
